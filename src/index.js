bd1e4c776a2dabec9da17e2e4a0285096cca5471
Costas <ceconomoujr@gmail.com>
2020-10-15 22:22:36 +0300

    Step 3

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var user = [];
+
 app.get('/', function(req,res) {
     res.send('Hello World!')
 })
 
 app.get('/users', function(req,res) {
-    res.send([])
+    res.send(user)
+})
+
+app.post('/users', function(req,res) {
+    user.push({id: 0})
+    res.json(user)
+})
+
+app.get('/users/:id', function(req,res) {
+    res.json(user)
 })
 
 app.listen(port,function(){console.log('server is running')})
